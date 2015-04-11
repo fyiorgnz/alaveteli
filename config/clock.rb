792e4d60d9befa4ea5436802dbd4b317433d1da1
Caleb <caleb.tutty@nzherald.co.nz>
2015-04-11 15:24:17 +1200

    Updated clock.rb to load rails env

--- config/clock.rb
+++ config/clock.rb
@@ -1,23 +1,19 @@
-require 'clockwork'
+require 'config/boot'
+require 'config/environment'
 
-module Clockwork
-  handler {}
+every(15.minutes, 'update_xapian_index') { UpdateXapianIndexWorker.perform_async }
 
-  every(15.minutes, 'update_xapian_index') { UpdateXapianIndexWorker.perform_async }
+# every(10.minutes, 'alert_tracks') { AlertTracksWorker.perform_async }
+every(10.minutes, 'send_batch_requests') { SendBatchRequestsWorker.perform_async }
 
-  # every(10.minutes, 'alert_tracks') { AlertTracksWorker.perform_async }
-  every(10.minutes, 'send_batch_requests') { SendBatchRequestsWorker.perform_async }
+every(10.minutes, 'process_s3_mail') { ProcessS3MailWorker.perform_async }
 
-  every(10.minutes, 'process_s3_mail') { ProcessS3MailWorker.perform_async }
+# every(1.hour, 'alert_comment_on_requests') { AlertCommentOnRequestsWorker.perform_async }
 
-  # every(1.hour, 'alert_comment_on_requests') { AlertCommentOnRequestsWorker.perform_async }
+# every(1.day, 'delete_old_things') { DeleteOldThingsWorker.perform_async }
+# every(1.day, 'alert_over_due_requests') { AlertOverdueRequestsWorker.perform_async }
+# every(1.day, 'alert_not_clarified_requests') { AlertNotClarifiedRequestsWorker.perform_async }
+# every(1.day, 'alert_not_clarified_request') { AlertOverdueRequestsWorker.perform_async }
+# every(1.day, 'stop_new_responses_on_old_requests') { StopNewResponsesOnOldRequestsWorker.perform_async }
 
-  # every(1.day, 'delete_old_things') { DeleteOldThingsWorker.perform_async }
-  # every(1.day, 'alert_over_due_requests') { AlertOverdueRequestsWorker.perform_async }
-  # every(1.day, 'alert_not_clarified_requests') { AlertNotClarifiedRequestsWorker.perform_async }
-  # every(1.day, 'alert_not_clarified_request') { AlertOverdueRequestsWorker.perform_async }
-  # every(1.day, 'stop_new_responses_on_old_requests') { StopNewResponsesOnOldRequestsWorker.perform_async }
-
-  every(1.day, 'update_public_bodies_stats') { UpdatePublicBodyStatsWorker.perform_async }
-
-end
+every(1.day, 'update_public_bodies_stats') { UpdatePublicBodyStatsWorker.perform_async }
