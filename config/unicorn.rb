f71627cd70b31bffa83f4fc25a4d2ca5bf0d3933
Caleb <caleb.tutty@nzherald.co.nz>
2015-04-14 18:25:36 +1200

    Fix to work with DATABASE_URL

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -27,9 +27,6 @@ after_fork do |server, worker|
   end
 
   defined?(ActiveRecord::Base) and
-    ActiveRecord::Base.establish_connection(
-      Rails.application.config.database_configuration[Rails.env]
-    )
-
+    ActiveRecord::Base.establish_connection
 end
 
