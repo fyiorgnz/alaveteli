8fc1b2f5dfe8e1cf15222ae65c229a4f2800e51e
Nigel Jones <njones@nigelj.nz>
2019-03-08 20:23:42 +1300

    Add logging to unicorn and PID file

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,3 +1,7 @@
+app_dir = File.expand_path("../..", __FILE__)
+shared_dir = (ENV['UNICORN_SHARED'] || "#{app_dir}/shared")
+working_directory app_dir
+
 # 8 workers and 1 master
 worker_processes(8)
 
@@ -8,6 +12,13 @@ timeout (ENV['UNICORN_TIMEOUT'] || 90).to_i
 
 # Listen on a Unix data socket
 listen ENV['UNICORN_SOCKET_PATH'], backlog: 2048
+
+# Logging
+stderr_path "#{shared_dir}/unicorn.stderr.log"
+stdout_path "#{shared_dir}/unicorn.stdout.log"
+
+# PID File
+pid "#{shared_dir}/unicorn.pid"
 
 before_fork do |server, worker|
 
